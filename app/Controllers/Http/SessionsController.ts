af919d5c44ef5ae2274eacb4a02e48b4c75d4a0b
Cristian Santos <cristiansantos.free@gmail.com>
2023-02-02 16:35:56 -0300

    Implemantado o envio de e-mail

--- app/Controllers/Http/SessionsController.ts
+++ app/Controllers/Http/SessionsController.ts
@@ -1,13 +1,22 @@
+import Mail from '@ioc:Adonis/Addons/Mail'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 
 export default class SessionsController {
 
   async store({ auth, request, response }: HttpContextContract) {
-    const { email, password } = request.all()
+    const { username, email, password } = request.all()
 
     try {
       const token = await auth.use('api').attempt(email, password)
+
+      await Mail.sendLater((message) => {
+        message
+          .from('contato@quinzeautomacao.com.br')
+          .to('cristiansantos.free@gmail.com')
+          .subject('Vc se inscreveu na api da Quinze!!!')
+          .htmlView('emails/welcome', { name: username, token: token.token })
+      })
       return token
     } catch (error) {
       return response.unauthorized('Invalid Credentials')
